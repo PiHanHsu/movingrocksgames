fcc704db5610a04e9b186cedbda9e4f1ad52c206
PiHan Hsu <pihanhsu@gmail.com>
2017-05-12 15:21:58 +0800

    finish 2 players game flow

--- Assets/Script/2Players/Players_Behavior.cs
+++ Assets/Script/2Players/Players_Behavior.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Players_Behavior : MonoBehaviour {
 
 	public GameObject ProtectBubble;
+	public GameObject HP_Level;
+	public GameObject HP_Text;
+
 
 	private float protectingTime;
+	private float hp_level= 100f;
+	private float whp;
 
 	// Use this for initialization
 	void Start () {
 		ProtectBubble.SetActive (false);
+		whp = HP_Level.transform.localScale.x * 0.1f;
 	}
 	
 	// Update is called once per frame
@@ -20,21 +26,28 @@ public class Players_Behavior : MonoBehaviour {
 
 		if (protectingTime > 8.0f) {
 			ProtectBubble.SetActive (false);
-
 		}
 		
 	}
 
 	void OnTriggerEnter2D (Collider2D col){
-		if (col.tag == "Bubble") {
-			ProtectBubble.SetActive (true);
-			protectingTime = 0;
-			Destroy (col.gameObject);
-		}
+		
+		if (Two_Players.isPlaying) {
+			if (col.tag == "Bubble") {
+				ProtectBubble.SetActive (true);
+				protectingTime = 0;
+				Destroy (col.gameObject);
+			}
 
-		if (col.tag == "Missile") {
-			Destroy (col.gameObject);
-			print ("hit by Missile");
+			if (col.tag == "Missile") {
+				Destroy (col.gameObject);
+				hp_level -= 10f;
+				HP_Text.GetComponent<TextMesh>().text = hp_level.ToString() + " / 100";
+				HP_Level.transform.localScale -= new Vector3 (whp, 0, 0);
+				if (hp_level < 1.0 ) {
+					Two_Players.isPlaying = false;
+				}
+			}
 		}
 	}
 }
--- Assets/Script/2Players/Two_Players.cs
+++ Assets/Script/2Players/Two_Players.cs
@@ -9,7 +9,18 @@ public class Two_Players : MonoBehaviour {
 	public GameObject Right_Missile;
 	public GameObject Right_Launcher;
 
+	public GameObject HP_Level_1P;
+	public GameObject HP_Text_1P;
+	public GameObject HP_Level_2P;
+	public GameObject HP_Text_2P;
+
+	public GameObject WinningText;
+	public GameObject NewGameText;
+
 	public GameObject Bubble;
+
+	public static bool isPlaying = true;
+	public static string winner;
 
 	private GameObject right_Missile;
 	private GameObject left_Missile;
@@ -27,22 +38,46 @@ public class Two_Players : MonoBehaviour {
 
 		bubble_time += Time.deltaTime;
 
-		if (bubble_time > 10.0f) {
-			bubble = Instantiate (Bubble, new Vector3 (0, -550, 0), Bubble.transform.rotation);
-			bubble_time = 0f;
-		}
+		if (isPlaying) {
+			NewGameText.SetActive (false);
+			WinningText.SetActive (false);
+			if (bubble_time > 10.0f) {
+				bubble = Instantiate (Bubble, new Vector3 (0, -550, 0), Bubble.transform.rotation);
+				bubble_time = 0f;
+			}
 
-		if (bubble != null) {
-			bubble.transform.position += new Vector3 (0, 10f, 0);
-		}
+			if (bubble != null) {
+				bubble.transform.position += new Vector3 (0, 10f, 0);
+			}
 
 
-		if(Input.GetKeyDown(KeyCode.Z)){
-			left_Missile = Instantiate (Left_Missile, Left_Launcher.gameObject.transform.position, transform.rotation);
-		}
+			if (Input.GetKeyDown (KeyCode.Z)) {
+				left_Missile = Instantiate (Left_Missile, new Vector3 (Left_Launcher.gameObject.transform.position.x + 100, Left_Launcher.gameObject.transform.position.y, 0), transform.rotation);
+			}
 
-		if(Input.GetKeyDown(KeyCode.Slash)){
-			right_Missile = Instantiate (Right_Missile, Right_Launcher.gameObject.transform.position, Right_Missile.transform.rotation);
+			if (Input.GetKeyDown (KeyCode.Slash)) {
+				right_Missile = Instantiate (Right_Missile, new Vector3 (Right_Launcher.gameObject.transform.position.x - 100, Right_Launcher.gameObject.transform.position.y, 0), Right_Missile.transform.rotation);
+			}
+		} else {
+			NewGameText.SetActive (true);
+			if (HP_Level_1P.transform.localScale.x > 0) {
+				WinningText.GetComponent<TextMesh> ().text = "The Winner is Mario";
+			} else {
+				WinningText.GetComponent<TextMesh> ().text = "The Winner is Luigi";
+			}
+			WinningText.SetActive (true);
 		}
+			
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			ResetGame ();
+		}
+	}
+
+	void ResetGame(){
+		isPlaying = true;
+		HP_Level_1P.transform.localScale = new Vector3 (300f, 200f, 1);
+		HP_Text_1P.GetComponent<TextMesh>().text = "100 / 100";
+		HP_Level_2P.transform.localScale = new Vector3 (300f, 200f, 1);
+		HP_Text_2P.GetComponent<TextMesh>().text = "100 / 100";
 	}
 }
